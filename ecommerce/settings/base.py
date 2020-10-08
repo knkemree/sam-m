8fd7366f4044d6468ad718b8429c1f95d6ba1985
knkemree <konakziyaemre@gmail.com>
2020-10-08 13:06:36 -0500

    settings

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -170,15 +170,15 @@ USE_TZ = True
 
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static_my_proj",
+    BASE_DIR4 / "static_my_proj",
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR4, 'static_cdn', 'static_root')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'media_root')
+MEDIA_ROOT = os.path.join(BASE_DIR4, 'static_cdn', 'media_root')
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = 'dashboard'
