5942eeeee53d016707c311639ceeab6122be8a1f
knkemree <konakziyaemre@gmail.com>
2020-10-08 12:14:23 -0500

    base

--- ecommerce/settings/pro.py
+++ ecommerce/settings/pro.py
@@ -1,20 +1,20 @@
 from .base import *
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'samnmdb',
-#         'USER': 'emre',
-#         'PASSWORD': 'Ziy@emre1992',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR3 / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'samnmdb',
+        'USER': 'emre',
+        'PASSWORD': 'Ziy@emre1992',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR3 / 'db.sqlite3',
+#     }
+# }
