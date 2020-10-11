878fedef853a3c5eb98f23e9483ff7a0b2f6fd00
knkemree <konakziyaemre@gmail.com>
2020-10-11 12:52:14 -0500

    bed_sheets, towels

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -55,12 +55,16 @@ def home_page(request):
         area_rugs = []
     
     try:
-        bed_sheets = Product.objects.filter(category__slug__contains="bed_sheets")
+        bed_sheet = Category.objects.get(slug__contains="bed-sheets")
+        bed_sheets = Product.objects.filter(category_id=bed_sheet.id)
     except:
         bed_sheets = []
+    print("bed_sheets")
+    print(bed_sheets)
 
     try:
-        towels = Product.objects.filter(category__slug__contains="towels")
+        towel = Category.objects.filter(slug__contains="towels")
+        towels = Product.objects.filter(category_id=towel.id)
     except:
         towels= []
 
