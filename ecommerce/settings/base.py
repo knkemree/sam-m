ce7d07653d3d316629c376433dd87ba971dcd685
knkemree <konakziyaemre@gmail.com>
2020-10-12 21:10:11 +0000

    celery settings

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    
+    'django_celery_beat',
+    'django_celery_results',
 
     
 
