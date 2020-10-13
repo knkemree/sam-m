151e0c2d5ecd2294938e63bdcf7e979789036c4d
knkemree <konakziyaemre@gmail.com>
2020-10-13 11:22:01 -0500

    nav menu categories

--- products/context_processor.py
+++ products/context_processor.py
@@ -1,6 +1,11 @@
 from .models import Product, Category
 
 def menu(request):
+    parent_categories = Category.objects.filter(active=True, parent=None)
+    child_categories1 = Category.objects.filter(active=True, parent__isnull=False)
+    
+    
+
     try:
         area_rug = Category.objects.get(slug__contains="area-rugs")
         area_rugs = Category.objects.filter(parent_id=area_rug.id)
@@ -20,8 +25,11 @@ def menu(request):
         towels = Product.objects.filter(category_id=towel.id)
     except:
         towels= []
-        
+
     return {'area_rugs': area_rugs,
             'bed_sheets': bed_sheets,
             'towels':towels,
+            'parent_categories': parent_categories,
+            'child_categories1':child_categories1,
+            
             }
