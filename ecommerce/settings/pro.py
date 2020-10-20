cafe7096a51214700aad780d6eb73956fdd52546
knkemree <konakziyaemre@gmail.com>
2020-10-20 08:35:22 -0500

    changes

--- ecommerce/settings/pro.py
+++ ecommerce/settings/pro.py
@@ -15,7 +15,7 @@ DATABASES = {
     }
 }
 
-STATIC_ROOT = "/var/www/msrugs.com/static/"
+#STATIC_ROOT = "/var/www/msrugs.com/static/"
 
 CELERY_BROKER_URL='redis://127.0.0.1:6379'
 CELERY_RESULT_BACKEND='redis://127.0.0.1:6379'
