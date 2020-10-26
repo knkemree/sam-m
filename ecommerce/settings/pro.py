8d8fef6516b5e8749ec96bf6b94bb8e9c759b88b
knkemree <konakziyaemre@gmail.com>
2020-10-26 12:03:18 -0500

    ckedior

--- ecommerce/settings/pro.py
+++ ecommerce/settings/pro.py
@@ -26,6 +26,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = 'samnm-static'
 AWS_DEFAULT_ACL = 'public-read'
+AWS_QUERYSTRING_AUTH = False
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
