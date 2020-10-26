4c5010ddfa15c57d67d55286ccc37638027e8405
knkemree <konakziyaemre@gmail.com>
2020-10-26 10:39:36 -0500

    ckeditor s3

--- ecommerce/settings/pro.py
+++ ecommerce/settings/pro.py
@@ -25,6 +25,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'samnm-static'
+AWS_QUERYSTRING_AUTH = False
 
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
