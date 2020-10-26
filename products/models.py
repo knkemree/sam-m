20f30e82f49f851f07767b59c21891b74bded163
knkemree <konakziyaemre@gmail.com>
2020-10-26 12:10:33 -0500

    remove ckeditor from model

--- products/models.py
+++ products/models.py
@@ -53,7 +53,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200, db_index=True) 
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, default= 'img/no_image.png')
     #description = RichTextField(blank=True, null=True) 
-    description = models.TextField(blank=True) 
+    description = models.TextField(blank=True, null=True) 
     #price = models.DecimalField(max_digits=10, decimal_places=2)
     #sale_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True) 
     #cost = models.DecimalField(max_digits=10, decimal_places=2)
