02328564e01b1d616869625a430dd9e0512297e6
knkemree <konakziyaemre@gmail.com>
2020-10-27 12:35:26 -0500

    ley decouple

--- payment/views.py
+++ payment/views.py
@@ -9,8 +9,8 @@ from orders.models import Order
 from django.template import loader
 
 #from orders.tasks import order_created, inform_admins
-stripe.api_key = "sk_test_LGKgGvfpnOtCepkfRQxOpFub"
-#stripe.api_key = config('stripe.api_key')
+#stripe.api_key = "sk_test_LGKgGvfpnOtCepkfRQxOpFub"
+stripe.api_key = config('STRIPE_PRIVATE_KEY')
 #STRIPE_PUB_KEY = 'pk_test_c6fSv46teTU4tycT1Aiv7ezy'
 
 STRIPE_PUBLIC_KEY = config('STRIPE_PUBLIC_KEY')
