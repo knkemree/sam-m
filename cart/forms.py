2206b3fd21b379c03568ec29f06baa9c9c1a5479
knkemree <konakziyaemre@gmail.com>
2020-10-29 09:02:17 -0500

    changed add tocar form css

--- cart/forms.py
+++ cart/forms.py
@@ -6,7 +6,7 @@ from coupons.models import Campaign
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 31)]
 class CartAddProductForm(forms.Form):
     
-    quantity = forms.IntegerField(widget=forms.NumberInput(attrs={'class': "col-xs-2"}))
+    quantity = forms.IntegerField(widget=forms.TextInput(attrs={}))
     #quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,coerce=int,#widget=forms.Select(attrs={'onchange':'this.form.submit();'}),)
     override = forms.BooleanField(required=False,
                                   initial=False,
