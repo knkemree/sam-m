759b2189e0bc648bae780efad343da6622e8c1af
knkemree <konakziyaemre@gmail.com>
2020-11-06 14:25:20 -0600

    celery documentation

--- ecommerce/celery.py
+++ ecommerce/celery.py
@@ -3,11 +3,28 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+# os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
 
-app = Celery('ecommerce')
+# app = Celery('ecommerce')
 
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+# app.config_from_object('django.conf:settings')
+# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+
+app = Celery('proj')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
