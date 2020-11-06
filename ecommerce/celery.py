a748a0aec3b36564f2528f55fb741fe05fb57a52
knkemree <konakziyaemre@gmail.com>
2020-11-06 14:25:58 -0600

    celery settings

--- ecommerce/celery.py
+++ ecommerce/celery.py
@@ -12,9 +12,9 @@ from django.conf import settings
 # app.config_from_object('django.conf:settings')
 # app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
-app = Celery('proj')
+app = Celery('ecommerce')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
