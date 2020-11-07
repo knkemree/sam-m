9feafedc66c5e36d9df7a5d57af155240701dd4f
knkemree <konakziyaemre@gmail.com>
2020-11-07 15:33:52 -0600

    send email to admins

--- account/tasks.py
+++ account/tasks.py
@@ -2,9 +2,7 @@ from celery import shared_task
 from django.core.mail import send_mail, mail_admins
 from django.template import loader
 from django.conf import settings
-from celery.utils.log import get_task_logger
 
-logger = get_task_logger(__name__)
 
 @shared_task
 def info_admins(email):
