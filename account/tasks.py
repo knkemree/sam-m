7a74c8ecde53a71f2a82889477c8744ff68084f5
knkemree <konakziyaemre@gmail.com>
2020-11-07 16:21:46 -0600

    send email fixing

--- account/tasks.py
+++ account/tasks.py
@@ -11,7 +11,7 @@ logger = get_task_logger(__name__)
 def info_admins(email):
     subject = "New Customer Registered"
     
-
-    mail_sent = mail_admins(subject, html_message="A new customer registered! Customer's email address is "+str(email))
+    message= "New customer"
+    mail_sent = mail_admins(subject, message, html_message="A new customer registered! Customer's email address is "+str(email))
 
     return mail_sent
