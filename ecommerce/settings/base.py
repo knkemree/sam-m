80998ccd335f1820de87fddac5e0cdb12bdedc67
knkemree <konakziyaemre@gmail.com>
2020-11-18 11:29:38 -0600

    debug tool bar config

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -273,3 +273,11 @@ CELERY_IMPORTS = (
     'account.tasks',
 )
 
+def show_toolbar(request):
+    return True
+
+
+DEBUG_TOOLBAR_CONFIG = {
+"SHOW_TOOLBAR_CALLBACK": show_toolbar,
+'INSERT_BEFORE': '</head>'
+}
