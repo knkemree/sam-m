136d93edf4dd7ac6a2ee79cd743e530bb1920744
knkemree <konakziyaemre@gmail.com>
2020-11-18 11:45:01 -0600

    ddtb show

--- ecommerce/settings/pro.py
+++ ecommerce/settings/pro.py
@@ -4,6 +4,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['165.22.33.200','msrugs.com','www.msrugs.com', 'www.samnmtrade.com', 'samnmtrade.com']
 #ALLOWED_HOSTS = ['msrugs.com','www.msrugs.com']
+INTERNAL_IPS = [
+    '165.22.33.200',
+]
+
+def custom_show_toolbar(request):
+    return True  # Always show toolbar, for example purposes only.
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': True,
+    'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
+
+}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -37,6 +49,3 @@ CELERY_RESULT_BACKEND='amqp://localhost'
 
 CKEDITOR_BASEPATH = "https://nyc3.digitaloceanspaces.com/samnm/samnm-static/ckeditor/ckeditor/"
 
-INTERNAL_IPS = [
-    'samnmtrade.com',
-]
