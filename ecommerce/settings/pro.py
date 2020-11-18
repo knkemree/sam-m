cfed04d21083eee2bc3e4f137f2a5706e11adcd3
knkemree <konakziyaemre@gmail.com>
2020-11-18 11:40:05 -0600

    djdtb settings

--- ecommerce/settings/pro.py
+++ ecommerce/settings/pro.py
@@ -38,5 +38,5 @@ CELERY_RESULT_BACKEND='amqp://localhost'
 CKEDITOR_BASEPATH = "https://nyc3.digitaloceanspaces.com/samnm/samnm-static/ckeditor/ckeditor/"
 
 INTERNAL_IPS = [
-    '165.22.33.200',
+    'samnmtrade.com',
 ]
