c7635c952ca28f3b3fa244074e0fc3455d9ac74b
knkemree <konakziyaemre@gmail.com>
2020-12-06 00:25:53 -0600

    speed optimization

--- products/context_processor.py
+++ products/context_processor.py
@@ -1,12 +1,12 @@
 from .models import Product, Category
 
 def menu(request):
-    parent_categories = Category.objects.filter(active=True, parent=None).select_related('parent')
-    child_categories1 = Category.objects.filter(active=True, parent__isnull=False).select_related('parent')
+    parent_categories = Category.objects.filter(active=True, parent=None)
+    child_categories1 = Category.objects.filter(active=True, parent__isnull=False)
 
     try:
         area_rug = Category.objects.get(slug__contains="area-rugs")
-        area_rugs = Category.objects.filter(parent_id=area_rug.id).select_related('parent')
+        area_rugs = Category.objects.filter(parent_id=area_rug.id)
     except:
         area_rugs = []
     
@@ -19,7 +19,7 @@ def menu(request):
     print(bed_sheets)
 
     try:
-        towel = Category.objects.filter(slug__contains="towels").select_related('parent')
+        towel = Category.objects.filter(slug__contains="towels")
         towels = Product.objects.filter(category_id=towel.id).select_related('category')
     except:
         towels= []
